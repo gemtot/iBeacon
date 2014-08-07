021c3f8050f1f3ed71c12bc7dea5bc783c1d2553
Nick <nick@passkit.com>
2014-08-07 12:20:04 +0800

    Xcode6 / iOS8 Beta5 Fixes
    Minor fixes for Xcode6 / iOS8 Beta5

--- GemTotSDK/GemTotSDK/Obejcts/GTBeaconBroadcaster.swift
+++ GemTotSDK/GemTotSDK/Obejcts/GTBeaconBroadcaster.swift
@@ -130,7 +130,7 @@ class GTBeaconBroadcaster: NSObject, CBPeripheralManagerDelegate {
             let region = CLBeaconRegion(proximityUUID:beaconUUID!, major:withMajor.unsignedShortValue, minor:withMinor.unsignedShortValue, identifier:"com.gemtots.afr")
             
             // Attempt to set up a peripheral with the measured power
-            let peripheralData = region.peripheralDataWithMeasuredPower(withPower === 127 ? nil : withPower)
+            let peripheralData = region.peripheralDataWithMeasuredPower((withPower.integerValue == 127) ? nil : withPower)
             // if we have a peripheral, start advertising
             if (peripheralData) {
                 
